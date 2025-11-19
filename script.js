062ae73fbb7964a55e0982096f730a2d23aa0b33
gulnar-hqvrd <gulnarhagverdiyeva18@gmail.com>
2025-11-19 14:18:53 +0400

    update

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-let tryCount = 3;
-let amount = 2000;
-let salary = 1000
-let UserPassword = "salam";
+let trycount = 3;
+let salary = 1000;
+let balance = 2000;
+let password = 1234;
 
 let isgetCredit = false;
+op1:while (trycount > 0) {
+  let Userpassword = +prompt("Passwordunuzu daxil edin");
 
-op1: while (tryCount > 0) {
-  let password = prompt("Passwordu daxil edin");
-
-  if (password == UserPassword) {
+  if (password == Userpassword) {
     while (true) {
       let isLogout = false;
       console.log(
-        "1)Balansa bax \n 2)Pul cixart 3)Hesabat al 4)Kredit al 5)Cixis"
+        "1)Balansa bax \n2)Pul cixart \n3)Hesabat al \n4)Kredit al \n5)Cixis"
       );
 
-      let choose = +prompt("Seçimizini daxil edin");
+      let choose = +prompt("Seçimizini daxil edin: Misal - 1");
 
       switch (choose) {
         case 1:
           showBalance();
           break;
         case 2:
-          takeMoney();
+          getMoney();
           break;
         case 3:
-          getTrm();
           break;
         case 4:
           getCredit();
@@ -33,46 +31,38 @@ op1: while (tryCount > 0) {
         case 5:
           isLogout = true;
           break;
-        default:
-          console.log("Duzgun secim etmediniz");
       }
       if (isLogout) {
         break op1;
       }
     }
   } else {
-    console.error("Parol sehvdir");
-    tryCount--;
+    console.log("Parolu duzgun daxil edin");
+    trycount--;
   }
-  if (tryCount == 0) {
-    console.log("Kartiniz bloklandi");
+  if (trycount == 0) {
+    console.log("kartiniz bloklandi");
+    break;
   }
 }
 
 function showBalance() {
-  console.log(`Balance : ${amount}`);
+  console.log(`Sizin Balansiniz : ${balance}`);
 }
 
-function takeMoney() {
-  if (amount <= 0) {
+function getMoney() {
+  if (balance <= 0) {
     console.log("Pulun yoxdur");
 
     return;
   }
-  let money = +prompt("Pul cekmek istediyiniz meblegi qeyd edin");
-  if (amount >= money) {
-    amount = amount - money;
-  } else if (money <= 0) {
-    console.log("Daxil etdiyiniz məbləğ 0 və - ola bilməz");
+  let money = +prompt("Meblegi daxil edin");
+  if (money > balance) {
+    console.log("Daxil etdiyiniz məbləğ büdcənizdən çoxdur");
   } else {
-    console.log("Balansinda kifayet qeder mebleg yoxdur");
+    balance = balance - money;
   }
   showBalance();
-}
-
-function getTrm() {
-    console.log("Gozlenilir");
-    
 }
 
 function getCredit() {
@@ -80,10 +70,12 @@ function getCredit() {
     console.log("Istifade edilin");
     return;
   }
-  amount = amount + salary * 0.45;
+  balance = balance + salary * 0.45;
   showBalance();
 
   isgetCredit = true;
 }
 
-function logout() {}
+function logOut() {
+  console.log("çıxış etdiniz");
+}
